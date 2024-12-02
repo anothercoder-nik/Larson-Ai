595dd4d5559b65cd8b8d7263b7d1b069aeccdced
anothercoder-nik <nikunjagarwal449@gmail.com>
2024-12-03 04:04:18 +0530

    final

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main/main"
+import Sidebar from "./components/Sidebar/Sidebar"
+
+const App = () => {
+  return (
+   <>
+   <Sidebar/>
+ <Main/>
+   </>
+  )
+}
+
+export default App
---
+++ src/config/gemini.js
@@ -0,0 +1,30 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const apiKey = "AIzaSyBiMTq5zOL62XNFRnWpqG7DFX6WYoNeEzE"; // Replace with your API key
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  console.log(result.response.text());
+  return response.text();
+}
+
+export default run;
